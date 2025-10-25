702005447f2c066387d7e77f7bfd3d4fd6db68ca
Jason <jasonrivera@Jasons-MacBook-Pro-2.local>
2025-10-25 09:57:14 -0700

    fix url

--- src/http/APIService.js
+++ src/http/APIService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 // Change the API_URL to the correct location of the backend API before deploying the app
-const API_URL = 'http://127.0.0.1:8000/'; /* 'http://localhost:8000' http://127.0.0.1:8000/ or  'http://yourPythonAnywhereName.pythonanywhere.com/'*/
+const API_URL = 'https://rivera1294.pythonanywhere.com'; /* 'http://localhost:8000' http://127.0.0.1:8000/ or  'http://yourPythonAnywhereName.pythonanywhere.com/'*/
 
 
   export class APIService {
